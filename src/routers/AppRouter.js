ae483d3a487778dcbc6d7cdc63a200114c417829
Ivan Hernandez Ruiz <musedelick70@gmail.com>
2021-09-01 19:59:26 -0500

    se añadio peticion al endpoint login -se añadio mensajes de error para el usuario en caso de no poder ingresar

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -7,6 +7,7 @@ import {
 } from "react-router-dom";
 import { DetailsDidactico } from "../views/DetailsDidactico";
 import { FormNewDidactico } from "../views/FormNewDidactico";
+import { Login } from "../views/Login";
 import { SearchDidactico } from "../views/SearchDidactico";
 
 export const AppRouter = () => {
@@ -22,6 +23,9 @@ export const AppRouter = () => {
                     </Route>
                     <Route exact path='/detalles'>
                         <DetailsDidactico tipo='mapa' numero='2' />
+                    </Route>
+                    <Route exact path="/login">
+                        <Login />
                     </Route>
                 </Switch>
             </div>
--- src/views/Login.js
+++ src/views/Login.js
@@ -1,19 +1,41 @@
-import { Field, Form, Formik } from 'formik'
-import React from 'react'
+import { ErrorMessage, Field, Form, Formik } from 'formik'
+import React, { useState } from 'react'
 import { InputText } from '../components/InputText'
 import { SeccionTitle } from '../components/SeccionTitle'
 import { SubmitButton } from '../components/SubmitButton'
+import { WarningLabel } from '../components/WarningLabel'
+import { apiUrl } from '../env/apiurl'
 import { schemaLogin } from '../helper/validationSchemas/schemaLogin'
 
 export const Login = () => {
+    const [errorToLogin, setErrorToLogin] = useState({error:false,message:""});
+    const {error,message}=errorToLogin
     const initialValues={
         username:"",
         password:""
     }
     
-    const handleSubmit=(values)=>{
+    const handleSubmit=async(values)=>{
         
-        console.log('me envian')
+        try{
+
+            const response = await fetch(`${apiUrl}/login`, {
+                method: 'POST', // *GET, POST, PUT, DELETE, etc.
+                mode: 'cors', // no-cors, *cors, same-origin
+                headers: {
+                  'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(values) // body data type must match "Content-Type" header
+            });
+            response.status===403&&setErrorToLogin({error:true,message:"Usuario y/o contraseña incorrecto"})
+            response.status===503&&setErrorToLogin({error:true,message:"Oops, tenemos un problema, intentalo mas tarde"})
+            
+            
+        }catch(e){
+            setErrorToLogin({error:true,message:"Oops, tenemos un problema, intentalo mas tarde"})
+            console.log(e)
+        }
+          
     }
     
     return (
@@ -21,7 +43,8 @@ export const Login = () => {
             <SeccionTitle title="Login"/>
             <Formik initialValues={initialValues} onSubmit={handleSubmit} validationSchema={schemaLogin}>
                 {({isSubmitting})=>(
-                    <Form className="mt-8  w-11/12	mx-auto rounded-lg px-4 py-8 flex flex-col bg-white space-y-2">
+                    <Form className="mt-8  w-11/12	mx-auto rounded-lg px-4 py-8 flex flex-col bg-white space-y-6">
+                        {error&&<WarningLabel forName="" warnMessage={message} /> }
                         <Field name="username" component={InputText} label="Usuario"  />
                         <Field name="password" component={InputText} label="Contraseña" isPassword  />
                         <SubmitButton text="Ingresar" onSubmitText="Ingresando" isSubmiting={isSubmitting}/>
